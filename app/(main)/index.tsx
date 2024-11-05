fd5b7039dac15aa2d04ed7b01c9d19c06175ca8a
msyafiqq <muhammad.syafiq@geniebook.com>
2024-11-06 02:18:38 +0700

    0003, attach todo catalogue

--- app/(main)/index.tsx
+++ app/(main)/index.tsx
@@ -1,6 +1,9 @@
+import {TodoCataloguePageStatic} from "@/components/page/todo/catalogue/todo-catalogue-page-static";
+
 export default function Menu0() {
   return (
     <>
+      <TodoCataloguePageStatic/>
     </>
   );
 }
--- app/(main)/menu-1.tsx
+++ app/(main)/menu-1.tsx
@@ -1,6 +1,9 @@
+import {TodoCataloguePageReactive} from "@/components/page/todo/catalogue/todo-catalogue-page-reactive";
+
 export default function Menu1() {
   return (
     <>
+      <TodoCataloguePageReactive/>
     </>
   );
 }
