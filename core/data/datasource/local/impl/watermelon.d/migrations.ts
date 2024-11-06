0128410faa86d1f2a8eef8b6003730140f8f8699
msyafiqq <muhammad.syafiq@geniebook.com>
2024-11-06 16:43:21 +0700

    0004, Migrations, add schema migration 2

--- core/data/datasource/local/impl/watermelon.d/migrations.ts
+++ core/data/datasource/local/impl/watermelon.d/migrations.ts
@@ -1,7 +1,18 @@
-import {schemaMigrations} from '@nozbe/watermelondb/Schema/migrations'
+import {addColumns, schemaMigrations} from '@nozbe/watermelondb/Schema/migrations'
 
 export default schemaMigrations({
   migrations: [
     //
+    {
+      toVersion: 2,
+      steps: [
+        addColumns({
+          table: 'todos',
+          columns: [
+            {name: 'picture', type: 'string', isOptional: true},
+          ],
+        }),
+      ],
+    },
   ],
 })
