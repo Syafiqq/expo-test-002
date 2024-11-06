8aa039bd6ebe0f1074cd2382db11c60dd972c614
msyafiqq <muhammad.syafiq@geniebook.com>
2024-11-06 19:25:15 +0700

    0004, Schema, increment to schema 2

--- core/data/datasource/local/impl/watermelon.d/schema.ts
+++ core/data/datasource/local/impl/watermelon.d/schema.ts
@@ -3,7 +3,7 @@ import todos from './schemas/todo'
 import populateInitial from './schemas/populate-initial'
 
 export default appSchema({
-  version: 1,
+  version: 2,
   tables: [
     todos.v2,
     populateInitial.v1,
