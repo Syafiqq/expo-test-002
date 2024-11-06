dfdfb1e36d08003079e9f3f622a2b543f342b253
msyafiqq <muhammad.syafiq@geniebook.com>
2024-11-06 09:39:18 +0700

    0003, attach direct component

--- app/(main)/menu-2.tsx
+++ app/(main)/menu-2.tsx
@@ -1,6 +1,9 @@
+import {TodoCataloguePageWithProvider} from "@/components/page/todo/catalogue/todo-catalogue-page-with-provider";
+
 export default function Menu2() {
   return (
     <>
+      <TodoCataloguePageWithProvider/>
     </>
   );
 }
