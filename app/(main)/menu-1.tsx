e8f4d0e6d36e5fac08f2ed680c916d94eb53a453
msyafiqq <muhammad.syafiq@geniebook.com>
2024-11-11 19:32:12 +0700

    0005: register drawer on menu 1

--- app/(main)/menu-1.tsx
+++ app/(main)/menu-1.tsx
@@ -1,9 +1,27 @@
+import React from 'react';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import Drawer, {DrawerForwardRef} from "@/components/page/todo/catalogue/components/drawer";
+import {Stack} from "expo-router";
+import {Button, View} from "react-native";
 import {TodoCataloguePageReactive} from "@/components/page/todo/catalogue/todo-catalogue-page-reactive";
 
 export default function Menu1() {
+  const drawerRef = React.useRef<DrawerForwardRef>(null);
+
   return (
-    <>
+    <GestureHandlerRootView style={{display: 'flex', flexGrow: 1}}>
+      <Stack.Screen
+        options={{
+          headerRight: () => (
+            <View style={{padding: 8}}>
+              <Button title="Toggle Drawer"
+                      onPress={() => drawerRef.current?.toggleDrawer()}/>
+            </View>
+          )
+        }}
+      />
       <TodoCataloguePageReactive/>
-    </>
+      <Drawer ref={drawerRef}/>
+    </GestureHandlerRootView>
   );
 }
